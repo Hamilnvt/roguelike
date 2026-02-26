eaad6a03eb2835ff20cfe379460c0d7d53e345c6
Mathieu Linty <mathieu.linty@studio.unibo.it>
2026-02-26 19:05:47 +0100

    Add nob commands

--- nob.c
+++ nob.c
@@ -5,14 +5,12 @@
 #define INCLUDE_DIR "include"
 #define BUILD_DIR "build"
 
-int main(int argc, char **argv)
+static inline bool streq(const char *s1, const char *s2) { return strcmp(s1, s2) == 0; }
+
+bool build(void)
 {
-    NOB_GO_REBUILD_URSELF(argc, argv);
-
-    if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
-
     File_Paths src_files = {0};
-    if (!read_entire_dir(SRC_DIR, &src_files)) return 1;
+    if (!read_entire_dir(SRC_DIR, &src_files)) return false;
 
     File_Paths include_files = {0};
     File_Paths headers = {0};
@@ -50,33 +48,82 @@ int main(int argc, char **argv)
             cmd_append(&cmd, "gcc", "-c", "-o", gcc_obj, input_path);
             cmd_append(&cmd, "-I./"INCLUDE_DIR, "-Wall", "-Wextra", "-Werror", "-Wswitch-enum", 
                              "-Wno-discarded-qualifiers", "-Wno-comment", "-ggdb");
-            if (!cmd_run(&cmd)) return 1;
+            if (!cmd_run(&cmd)) return false;
         }
 
         if (needs_rebuild(clang_obj, deps.items, deps.count)) {
             cmd_append(&cmd, "clang", "-c", "-o", clang_obj, input_path);
             cmd_append(&cmd, "-I./"INCLUDE_DIR, "-Wall", "-Wextra", "-Werror", "-Wswitch-enum", 
                              "-Wno-unused-function", "-ggdb");
-            if (!cmd_run(&cmd)) return 1;
+            if (!cmd_run(&cmd)) return false;
         }
 
         da_free(deps);
     }
 
-    const char *gcc_exe = BUILD_DIR"/roguelike";
+    const char *gcc_exe = BUILD_DIR"/roguelike.gcc";
     if (needs_rebuild(gcc_exe, gcc_objs.items, gcc_objs.count)) {
         cmd_append(&cmd, "gcc", "-o", gcc_exe);
         da_append_many(&cmd, gcc_objs.items, gcc_objs.count);
         cmd_append(&cmd, "-lncurses", "-lm");
-        if (!cmd_run(&cmd)) return 1;
+        if (!cmd_run(&cmd)) return false;
     }
 
-    const char *clang_exe = BUILD_DIR"/clang_roguelike";
+    const char *clang_exe = BUILD_DIR"/roguelike.clang";
     if (needs_rebuild(clang_exe, clang_objs.items, clang_objs.count)) {
         cmd_append(&cmd, "clang", "-o", clang_exe);
         da_append_many(&cmd, clang_objs.items, clang_objs.count);
         cmd_append(&cmd, "-lncurses", "-lm");
-        if (!cmd_run(&cmd)) return 1;
+        if (!cmd_run(&cmd)) return false;
+    }
+
+    return true;
+}
+
+bool run(void)
+{
+    Cmd cmd = {0};
+    cmd_append(&cmd, "./build/roguelike.gcc");
+    return cmd_run(&cmd);
+}
+
+bool delete_file(Nob_Walk_Entry entry);
+
+bool clean(void)
+{
+    Nob_File_Paths build_files = {0}
+    if (!nob_read_entire_dir(BUILD_DIR, &build_files)) return false;
+    da_foreach (const char *, file, &build_files) {
+        if (!delete_file(*file)) return false;
+    }
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    const char *program_name = shift(argv, argc);
+
+    if (argc == 0) {
+        nob_log(ERROR, "Usage: %s <command>", program_name);
+        // TODO: list possible commands
+        return 1;
+    }
+
+    if (!mkdir_if_not_exists(BUILD_DIR)) return 1;
+    const char *command = shift(argv, argc);
+
+    if (streq(command, "build")) {
+        if (!build()) return 1;
+    } else if (streq(command, "run")) {
+        if (!build()) return 1;
+        if (!run()) return 1;
+    } else if (streq(command, "clean")) {
+        if (!clean()) return 1;
+    } else {
+        nob_log(ERROR, "Unknown command `%s`", command);
+        return 1;
     }
 
     return 0;
