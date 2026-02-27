f93a63d1c500d69d69b7602434d3f1cf923facbb
Mathieu Linty <mathieu.linty@studio.unibo.it>
2026-02-27 10:33:14 +0100

    Remove old main and old way of building

--- old_build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-set -e
-
-clear
-
-gcc   -o old_roguelike       old_main.c -lncurses -lm -Wall -Wextra -Werror -Wswitch-enum -Wno-discarded-qualifiers -ggdb
-clang -o old_clang_roguelike old_main.c -lncurses -lm -Wall -Wextra -Werror -Wswitch-enum -Wno-unused-function -ggdb
